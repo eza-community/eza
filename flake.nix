05df8c9fea9c7718b77511273711f380b36c0b74
Christina Sørensen <christina@cafkafk.com>
2023-07-29 18:23:14 +0200

    ci(nix): add treefmt, rust-toolchain, nixfmt
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- flake.nix
+++ flake.nix
@@ -1,21 +1,85 @@
 {
+
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    naersk.url = "github:nix-community/naersk";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+  outputs = {
+      self
+    , flake-utils
+    , naersk
+    , nixpkgs
+    , treefmt-nix
+    , rust-overlay
+  }:
+    flake-utils.lib.eachDefaultSystem (
+
+      system: let
+
+        overlays = [(import rust-overlay)];
+
+        pkgs = (import nixpkgs) {
+
+          inherit system overlays;
+
         };
-      });
+
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        naersk' = pkgs.callPackage naersk {
+
+          cargo = toolchain;
+
+          rustc = toolchain;
+
+        };
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+
+      in {
+
+        # For `nix fmt`
+
+        formatter = treefmtEval.config.build.wrapper;
+
+        # For `nix build` & `nix run`:
+
+        packages.default = naersk'.buildPackage {
+
+          src = ./.;
+
+        };
+
+        # For `nix develop`:
+
+        devShells.default = pkgs.mkShell {
+
+          nativeBuildInputs = with pkgs; [toolchain];
+
+        };
+
+        # for `nix flake check`
+
+        checks = {
+
+          formatting = treefmtEval.config.build.check self;
+
+        };
+
+      }
+
+    );
+
 }
+
+
