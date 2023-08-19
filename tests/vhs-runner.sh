78c11b51d4725572a891b16495209a9b08a7e04c
Christina Sørensen <christina@cafkafk.com>
2023-08-19 17:00:04 +0200

    build(vhs): add print_msg with ansi color
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- tests/vhs-runner.sh
+++ tests/vhs-runner.sh
@@ -5,20 +5,33 @@ set TAPES $TEST_DIR/tapes
 set REFERENCES $TEST_DIR/references
 set TEMP $TEST_DIR/tmp
 
+function print_msg -a ARG -a OP -a NAME -a MSG
+    set_color reset
+    echo -n "[$OP] "
+    set_color reset
+    set_color -b $ARG
+    set_color 000
+    echo -n "[ $NAME ]:"
+    set_color reset
+    echo " $MSG"
+    set_color reset
+end
+
 function run_test -d "Run VHS test" -a NAME
 
     set NAME_TAPE "$NAME.tape"
 
-    set SUCCESS "[+] $NAME: Success"
-    set FAILURE "[+] $NAME: Failure"
+    set SUCCESS (print_msg "0D0" "+" "$NAME" "Success")
+    set FAILURE (print_msg "D00" "-" "$NAME" "Failure")
 
     set GEN_DIR $TEMP
     set GEN_FILE $GEN_DIR/$NAME.txt
     set GEN_FILE_ESCAPE (echo $GEN_FILE | sed "s/\//\\\\\//g")
 
-    echo "[*] Testing $NAME..."
+    echo "[*] $NAME: Testing..."
+    print_msg DD0 "*" $NAME "Testing..."
 
-    cat $TAPES/$NAME_TAPE | sed s/outfile/$GEN_FILE_ESCAPE/ | sed s/-l// | vhs >/dev/null
+    cat $TAPES/$NAME_TAPE | sed s/outfile/$GEN_FILE_ESCAPE/ | sed s/-l// | vhs &>/dev/null
 
     cmp -s -- $REFERENCES/$NAME.txt $TEMP/$NAME.txt && echo $SUCCESS || echo $FAILURE
 end
@@ -27,7 +40,7 @@ function gen_test -d "Generate VHS test" -a NAME
 
     set NAME_TAPE "$NAME.tape"
 
-    set SUCCESS "[+] $NAME: Success"
+    set SUCCESS (set_color yellow; echo -n "[+] $NAME:"; set_color reset; echo " Success")
     set FAILURE "[+] $NAME: Failure"
 
     set GEN_DIR $REFERENCES
@@ -38,13 +51,13 @@ function gen_test -d "Generate VHS test" -a NAME
     # to change the reference. They should now only have to delete the old
     # reference, and a new one will be generated.
     if builtin test -f $GEN_FILE
-        echo "[*] $GEN_FILE exists, skipping generating it"
+        echo "[+] $NAME: $GEN_FILE exists, skipping gen"
         return
     end
 
-    echo "[*] Generating $NAME..."
+    echo "[*] $NAME: Testing..."
 
-    cat $TAPES/$NAME_TAPE | sed s/outfile/$GEN_FILE_ESCAPE/ | sed s/-l// | vhs >/dev/null
+    cat $TAPES/$NAME_TAPE | sed s/outfile/$GEN_FILE_ESCAPE/ | sed s/-l// | vhs &>/dev/null
 
     cmp -s -- $REFERENCES/$NAME.txt $TEMP/$NAME.txt && echo $SUCCESS || echo $FAILURE
 end
