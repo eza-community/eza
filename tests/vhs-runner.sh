a3740e759d9ea14543579161557c11ff04b9d7fa
Christina Sørensen <christina@cafkafk.com>
2023-08-19 15:17:27 +0200

    build(vhs): add test runner sketch
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

---
+++ tests/vhs-runner.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env fish
+
+set TEST_DIR tests
+set TAPES "$TEST_DIR/tapes"
+set REFERENCES "$TEST_DIR\/references"
+set TEMP "$TEST_DIR\/tmp"
+
+function run_test -d "Run VHS tests"
+
+    set NAME main
+    set NAME_TAPE "$NAME.tape"
+
+    set SUCCESS "[+] $NAME: Success"
+    set FAILURE "[+] $NAME: Failure"
+
+    echo "[*] Testing $NAME..."
+
+    cat $TAPES/$NAME_TAPE | sed "s/outfile/$TEMP\/$NAME.txt/" | sed s/-l// | vhs
+    # diff -q validated.ascii validated.txt && echo $SUCCESS || echo $FAILURE
+    cmp -s -- $REFERENCES/$NAME.txt $TEMP/$NAME.txt && echo $SUCCESS || echo $FAILURE
+end
+
+run_test
