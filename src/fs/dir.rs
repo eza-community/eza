e7a50d33fd341a399c635a31cd588a2baff661f3
Christina Sørensen <christina@cafkafk.com>
2023-09-14 16:29:29 +0200

    fix(debug): tracing typo
    Co-authored-by: Christina Sørensen <christina@cafkafk.com>
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- src/fs/dir.rs
+++ src/fs/dir.rs
@@ -42,7 +42,7 @@ impl Dir {
                           .map(|result| result.map(|entry| entry.path()))
                           .collect::<Result<_, _>>()?;
 
-        info!("Read directory succes {:?}", &path);
+        info!("Read directory success {:?}", &path);
         Ok(Self { contents, path })
     }
 
