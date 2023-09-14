27d96dd2de860b647f828e9e65f7cc7038bdf8e7
MartinFillon <martin.fillon@hotmail.com>
2023-09-14 18:56:56 +0200

    chore(test_generator): run spellcheck

--- devtools/generate-trycmd-test.sh
+++ devtools/generate-trycmd-test.sh
@@ -9,32 +9,32 @@ fi
 
 # Clean up previous test data
 
-if [ -f tests/cmd/$1.toml ]; then
-    rm tests/cmd/$1.toml
+if [ -f tests/cmd/"$1".toml ]; then
+    rm tests/cmd/"$1".toml
 fi
 
-if [ -f tests/cmd/$1.stdout ]; then
-    rm tests/cmd/$1.stdout
+if [ -f tests/cmd/"$1".stdout ]; then
+    rm tests/cmd/"$1".stdout
 fi
 
-if [ -f tests/cmd/$1.stderr ]; then
-    rm tests/cmd/$1.stderr
+if [ -f tests/cmd/"$1".stderr ]; then
+    rm tests/cmd/"$1".stderr
 fi
 
 # Generate test data
 
-touch tests/cmd/$1.toml
+touch tests/cmd/"$1".toml
 
-echo 'bin.name = "eza"' >> tests/cmd/$1.toml
-echo 'args = "'$2'"' >> tests/cmd/$1.toml
+echo 'bin.name = "eza"' >> tests/cmd/"$1".toml
+echo 'args = "'"$2"'"' >> tests/cmd/"$1".toml
 
 # Generate expected output
 
 if [ -f target/debug/eza ]; then
-    target/debug/eza $2 > tests/cmd/$1.stdout 2> tests/cmd/$1.stderr
+    target/debug/eza "$2" > tests/cmd/"$1".stdout 2> tests/cmd/"$1".stderr
     returncode=$?
     if [ $returncode -ne 0 ]; then
-        echo -e 'status.code = '$returncode'' >> tests/cmd/$1.toml
+        echo -e 'status.code = '$returncode'' >> tests/cmd/"$1".toml
         exit 0
     fi
 else
