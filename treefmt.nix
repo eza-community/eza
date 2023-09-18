5fba960c5af4724652ec32198fb189838acfaab3
9glenda <plan9git@proton.me>
2023-09-18 23:22:43 +0200

    ci: added formatters to treefmt
    - yaml: for GitHub Actions
    - toml: for Rust configs mainly
    - deadnix: detecting dead nix code

--- treefmt.nix
+++ treefmt.nix
@@ -1,9 +1,12 @@
 {
   projectRootFile = "Cargo.toml";
   programs = {
-    alejandra.enable = true;
-    rustfmt.enable = true;
-    shellcheck.enable = true;
+    alejandra.enable = true; # nix
+    rustfmt.enable = true; # rust
+    shellcheck.enable = true; # bash/shell
+    deadnix.enable = true; # find dead nix code
+    taplo.enable = true; # toml
+    yamlfmt.enable = true; # yaml
   };
   settings = {
     formatter.shellcheck.includes = ["*.sh" "./completions/bash/eza"];
