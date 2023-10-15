2e0d0158835d7ac979181cc8e154c7683953c93c
xempt <99044751+Xemptuous@users.noreply.github.com>
2023-10-15 15:18:15 -0700

    implemented global mut hashmap for sizes

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -3,6 +3,13 @@ pub use self::dir::{Dir, DotFilter};
 
 mod file;
 pub use self::file::{File, FileTarget};
+use std::sync::Mutex;
+use std::collections::HashMap;
+
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref RECURSIVE_SIZE_HASHMAP: Mutex<HashMap<u64, u64>> = Mutex::new(HashMap::new());
+}
 
 pub mod dir_action;
 pub mod feature;
