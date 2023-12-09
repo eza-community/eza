d18c007772e859133feb08023b1e3edd5f84bef8
Erwin van Eijk <235739+erwinvaneijk@users.noreply.github.com>
2023-12-09 21:34:44 +0100

    feat(time): time display use /etc/timezone
    When a file timestamp is updated under daylight saving time, and
    displayed when it is not, the wrong time offset is displayed (and vice
    versa).
    
    Fixes issue #653.

--- src/output/render/times.rs
+++ src/output/render/times.rs
@@ -5,12 +5,15 @@ use ansiterm::Style;
 use chrono::prelude::*;
 
 pub trait Render {
-    fn render(self, style: Style, time_offset: FixedOffset, time_format: TimeFormat) -> TextCell;
+    fn render(self, style: Style, time_format: TimeFormat) -> TextCell;
 }
 
 impl Render for Option<NaiveDateTime> {
-    fn render(self, style: Style, time_offset: FixedOffset, time_format: TimeFormat) -> TextCell {
+    fn render(self, style: Style, time_format: TimeFormat) -> TextCell {
+        let timezone_str = iana_time_zone::get_timezone().unwrap();
+        let timezone: chrono_tz::Tz = timezone_str.parse().unwrap();
         let datestamp = if let Some(time) = self {
+            let time_offset = timezone.offset_from_utc_datetime(&time).fix();
             time_format.format(&DateTime::<FixedOffset>::from_naive_utc_and_offset(
                 time,
                 time_offset,
