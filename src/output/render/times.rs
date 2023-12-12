26f206968fce6ff5c07e436bae6d71de18a8fbd3
Erwin van Eijk <235739+erwinvaneijk@users.noreply.github.com>
2023-12-12 21:39:34 +0100

    feat(time_display): time should use /etc/timezone
    When a file timestamp is displayed, it should use the timezone info from
    the time of the timestamp, not from when it is displayed. This occurs
    when a file timestamp is updated under daylight saving time, and
    displayed at a time when not under daylight saving time.

--- src/output/render/times.rs
+++ src/output/render/times.rs
@@ -11,12 +11,15 @@ use chrono::prelude::*;
 use nu_ansi_term::Style;
 
 pub trait Render {
-    fn render(self, style: Style, time_offset: FixedOffset, time_format: TimeFormat) -> TextCell;
+    fn render(self, style: Style, time_format: TimeFormat) -> TextCell;
 }
 
 impl Render for Option<NaiveDateTime> {
-    fn render(self, style: Style, time_offset: FixedOffset, time_format: TimeFormat) -> TextCell {
+    fn render(self, style: Style, time_format: TimeFormat) -> TextCell {
+        let timezone_str = iana_time_zone::get_timezone().unwrap();
+        let timezone: chrono_tz::Tz = timezone_str.parse().unwrap();
         let datestamp = if let Some(time) = self {
+            let time_offset = timezone.offset_from_utc_datetime(&time).fix();
             time_format.format(&DateTime::<FixedOffset>::from_naive_utc_and_offset(
                 time,
                 time_offset,
