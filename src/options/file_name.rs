3099cb59daaf38a5e861df08f00065d8b8b05819
Terts Diepraam <terts.diepraam@gmail.com>
2024-02-05 16:55:59 +0100

    refactor: remove unnecessary types and implementations from parser.rs

--- src/options/file_name.rs
+++ src/options/file_name.rs
@@ -188,7 +188,7 @@ mod tests {
     #[test]
     fn deduce_show_icon_always() {
         let options = Opts {
-            icons: Some(clap::builder::OsStr::from("always").into()),
+            icons: Some(ShowWhen::Always),
             ..Opts::default()
         };
 
@@ -202,7 +202,7 @@ mod tests {
     #[test]
     fn deduce_show_icons_never() {
         let options = Opts {
-            icons: Some(clap::builder::OsStr::from("never").into()),
+            icons: Some(ShowWhen::Never),
             ..Opts::default()
         };
 
