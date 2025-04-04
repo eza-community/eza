eee95802b89947e72508c65c2649f5e86a08b93d
ariasuni <aria@ariasuni.eu>
2025-04-04 02:08:26 +0200

    feat(clap): WIP
    Co-authored-by: PThorpe92 <preston@unlockedlabs.org>
    Co-authored-by: MartinFillon <martin.fillon@hotmail.com>
    Co-authored-by: Terts Diepraam <terts.diepraam@gmail.com>

--- .config/treefmt.nix
+++ .config/treefmt.nix
@@ -20,7 +20,6 @@
         ];
         excludes = [ ".envrc" ];
       };
-      rustfmt.excludes = [ "src/options/flags.rs" ];
       taplo.excludes = [ "tests/ptests/*.toml" ];
       yamlfmt.excludes = [ "./powertest.yaml" ];
     };
