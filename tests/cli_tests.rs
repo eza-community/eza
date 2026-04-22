a1624c6869cdd965fadf5d0943549a44b54a9383
ariasuni <aria@ariasuni.eu>
2026-04-22 18:11:19 +0200

    Makes locale date tests unix-only since they depend on locales

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -33,101 +33,6 @@ fn cli_tests_any_dotfiles() {
 }
 
 #[test]
-#[cfg(unix)]
-#[cfg(feature = "git")]
-fn cli_tests_any_git() {
-    use std::io::Write;
-
-    let test_dir = TestDirectory::create("unix", "git");
-    test_dir.run("git", &["init", "."]);
-    test_dir.create_dirs(&[
-        "dir-empty",
-        "dir-ignored",
-        "dir-ignored-with-file-unmodified",
-        "dir-modified",
-        "dir-modified-staged",
-        "dir-new",
-        "dir-new-staged",
-        "dir-new-staged-new",
-    ]);
-
-    test_dir.create_files(&[
-        "dir-ignored/file_new",
-        "dir-ignored-with-file-unmodified/file-unmodified",
-        "dir-new-staged-new/file-new-staged",
-        "dir-new/file-ignored",
-        "dir-new/file-new",
-        "file-ignored",
-        "file-new",
-        "file-new-staged",
-    ]);
-    let mut dir_modified = test_dir.create_file("dir-modified/file-modified");
-    let mut dir_modified_staged = test_dir.create_file("dir-modified-staged/file-modified-staged");
-    let mut modified = test_dir.create_file("file-modified");
-    let mut modified_staged = test_dir.create_file("file-modified-staged");
-    let mut modified_staged_modified = test_dir.create_file("file-modified-staged-modified");
-    let mut new_staged_modified = test_dir.create_file("file-new-staged-modified");
-
-    test_dir.symlink("file-modified", "symlink-new");
-    test_dir.symlink("file-non-existing", "symlink-broken");
-
-    let mut gitignore = test_dir.create_file(".gitignore");
-    gitignore.write_all(b"dir-ignored*\nfile-ignored*").unwrap();
-
-    test_dir.run("git", &["config", "user.name", "Eza"]);
-    test_dir.run("git", &["config", "user.email", "eza@eza.test"]);
-
-    test_dir.run(
-        "git",
-        &[
-            "add",
-            "dir-modified",
-            "dir-modified-staged",
-            "file-modified",
-            "file-modified-staged",
-            "file-modified-staged-modified",
-        ],
-    );
-    test_dir.run(
-        "git",
-        &[
-            "add",
-            "-f",
-            "dir-ignored-with-file-unmodified/file-unmodified",
-        ],
-    );
-    test_dir.run("git", &["commit", "-m", "initial commit"]);
-
-    modified.write_all(b"a").unwrap();
-    modified_staged.write_all(b"a").unwrap();
-    modified_staged_modified.write_all(b"a").unwrap();
-    dir_modified.write_all(b"a").unwrap();
-    dir_modified_staged.write_all(b"a").unwrap();
-
-    test_dir.run(
-        "git",
-        &[
-            "add",
-            "dir-modified-staged",
-            "dir-new-staged",
-            "dir-new-staged-new",
-            "file-modified-staged",
-            "file-modified-staged-modified",
-            "file-new-staged",
-            "file-new-staged-modified",
-            "dir-new-staged-new/file-new-staged",
-        ],
-    );
-
-    test_dir.create_file("dir-new-staged-new/file-new");
-
-    new_staged_modified.write_all(b"a").unwrap();
-    modified_staged_modified.write_all(b"a").unwrap();
-
-    test_dir.run_tests();
-}
-
-#[test]
 #[cfg(not(feature = "git"))]
 fn cli_tests_any_no_git() {
     let test_dir = TestDirectory::create("any", "no-git");
@@ -137,46 +42,12 @@ fn cli_tests_any_no_git() {
 }
 
 #[test]
-// This test needs locales en_US, fr_FR and ja_JP.
-fn cli_tests_any_date() {
-    use std::fs::FileTimes;
-    use std::thread;
-    use std::time::{Duration, SystemTime};
+fn cli_tests_any_sort() {
+    let test_dir = TestDirectory::create("any", "sort");
 
-    use chrono::{Local, TimeZone};
+    test_dir.create_files(&["a.txt", "abc.mp3", "ab"]);
 
-    let test_dir = TestDirectory::create("any", "dates");
-
-    let old_date: SystemTime = Local.with_ymd_and_hms(2003, 3, 3, 0, 0, 0).unwrap().into();
-    let med_date: SystemTime = Local
-        .with_ymd_and_hms(2006, 6, 15, 23, 14, 29)
-        .unwrap()
-        .into();
-    let new_date: SystemTime = Local
-        .with_ymd_and_hms(2009, 12, 22, 10, 38, 53)
-        .unwrap()
-        .into();
-
-    // Sleep between each create as we can not modified the created time
-    let peach = test_dir.create_file("peach");
-    thread::sleep(Duration::from_millis(100));
-    let peach_times = FileTimes::new()
-        .set_modified(med_date)
-        .set_accessed(new_date);
-    peach.set_times(peach_times).unwrap();
-
-    let plum = test_dir.create_file("plum");
-    thread::sleep(Duration::from_millis(100));
-    let plum_times = FileTimes::new()
-        .set_modified(new_date)
-        .set_accessed(old_date);
-    plum.set_times(plum_times).unwrap();
-
-    let pear = test_dir.create_file("pear");
-    let pear_times = FileTimes::new()
-        .set_modified(old_date)
-        .set_accessed(med_date);
-    pear.set_times(pear_times).unwrap();
+    test_dir.create_dirs(&["test", "abc", "01.city", "02.apple"]);
 
     test_dir.run_tests();
 }
---
+++ tests/cli_tests_unix.rs
@@ -0,0 +1,144 @@
+mod cli_tests_helpers;
+
+use cli_tests_helpers::TestDirectory;
+
+#[test]
+#[cfg(unix)]
+// This test needs locales en_US, fr_FR and ja_JP.
+fn cli_tests_unix_date() {
+    use std::fs::FileTimes;
+    use std::thread;
+    use std::time::{Duration, SystemTime};
+
+    use chrono::{Local, TimeZone};
+
+    let test_dir = TestDirectory::create("any", "dates");
+
+    let old_date: SystemTime = Local.with_ymd_and_hms(2003, 3, 3, 0, 0, 0).unwrap().into();
+    let med_date: SystemTime = Local
+        .with_ymd_and_hms(2006, 6, 15, 23, 14, 29)
+        .unwrap()
+        .into();
+    let new_date: SystemTime = Local
+        .with_ymd_and_hms(2009, 12, 22, 10, 38, 53)
+        .unwrap()
+        .into();
+
+    // Sleep between each create as we can not modified the created time
+    let peach = test_dir.create_file("peach");
+    thread::sleep(Duration::from_millis(100));
+    let peach_times = FileTimes::new()
+        .set_modified(med_date)
+        .set_accessed(new_date);
+    peach.set_times(peach_times).unwrap();
+
+    let plum = test_dir.create_file("plum");
+    thread::sleep(Duration::from_millis(100));
+    let plum_times = FileTimes::new()
+        .set_modified(new_date)
+        .set_accessed(old_date);
+    plum.set_times(plum_times).unwrap();
+
+    let pear = test_dir.create_file("pear");
+    let pear_times = FileTimes::new()
+        .set_modified(old_date)
+        .set_accessed(med_date);
+    pear.set_times(pear_times).unwrap();
+
+    test_dir.run_tests();
+}
+
+#[test]
+#[cfg(unix)]
+#[cfg(feature = "git")]
+fn cli_tests_unix_git() {
+    use std::io::Write;
+
+    let test_dir = TestDirectory::create("unix", "git");
+    test_dir.run("git", &["init", "."]);
+    test_dir.create_dirs(&[
+        "dir-empty",
+        "dir-ignored",
+        "dir-ignored-with-file-unmodified",
+        "dir-modified",
+        "dir-modified-staged",
+        "dir-new",
+        "dir-new-staged",
+        "dir-new-staged-new",
+    ]);
+
+    test_dir.create_files(&[
+        "dir-ignored/file_new",
+        "dir-ignored-with-file-unmodified/file-unmodified",
+        "dir-new-staged-new/file-new-staged",
+        "dir-new/file-ignored",
+        "dir-new/file-new",
+        "file-ignored",
+        "file-new",
+        "file-new-staged",
+    ]);
+    let mut dir_modified = test_dir.create_file("dir-modified/file-modified");
+    let mut dir_modified_staged = test_dir.create_file("dir-modified-staged/file-modified-staged");
+    let mut modified = test_dir.create_file("file-modified");
+    let mut modified_staged = test_dir.create_file("file-modified-staged");
+    let mut modified_staged_modified = test_dir.create_file("file-modified-staged-modified");
+    let mut new_staged_modified = test_dir.create_file("file-new-staged-modified");
+
+    test_dir.symlink("file-modified", "symlink-new");
+    test_dir.symlink("file-non-existing", "symlink-broken");
+
+    let mut gitignore = test_dir.create_file(".gitignore");
+    gitignore.write_all(b"dir-ignored*\nfile-ignored*").unwrap();
+
+    test_dir.run("git", &["config", "user.name", "Eza"]);
+    test_dir.run("git", &["config", "user.email", "eza@eza.test"]);
+
+    test_dir.run(
+        "git",
+        &[
+            "add",
+            "dir-modified",
+            "dir-modified-staged",
+            "file-modified",
+            "file-modified-staged",
+            "file-modified-staged-modified",
+        ],
+    );
+    test_dir.run(
+        "git",
+        &[
+            "add",
+            "-f",
+            "dir-ignored-with-file-unmodified/file-unmodified",
+        ],
+    );
+    test_dir.run("git", &["commit", "-m", "initial commit"]);
+
+    modified.write_all(b"a").unwrap();
+    modified_staged.write_all(b"a").unwrap();
+    modified_staged_modified.write_all(b"a").unwrap();
+    dir_modified.write_all(b"a").unwrap();
+    dir_modified_staged.write_all(b"a").unwrap();
+
+    test_dir.run(
+        "git",
+        &[
+            "add",
+            "dir-modified-staged",
+            "dir-new-staged",
+            "dir-new-staged-new",
+            "file-modified-staged",
+            "file-modified-staged-modified",
+            "file-new-staged",
+            "file-new-staged-modified",
+            "dir-new-staged-new/file-new-staged",
+        ],
+    );
+
+    test_dir.create_file("dir-new-staged-new/file-new");
+
+    new_staged_modified.write_all(b"a").unwrap();
+    modified_staged_modified.write_all(b"a").unwrap();
+
+    test_dir.run_tests();
+}
