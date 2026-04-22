67baa2f5c9e7647f31f604311c3ab43f7b534c0c
ariasuni <aria@ariasuni.eu>
2026-04-22 22:59:13 +0200

    Mark some cli tests as Linux rather (incorrectly) unix

---
+++ tests/cli_tests_linux.rs
@@ -0,0 +1,79 @@
+mod cli_tests_helpers;
+
+use cli_tests_helpers::TestDirectory;
+
+#[test]
+#[cfg(target_os = "linux")]
+// This test needs locales en_US, fr_FR and ja_JP.
+fn cli_tests_linux_date() {
+    use std::fs::FileTimes;
+    use std::thread;
+    use std::time::{Duration, SystemTime};
+
+    use chrono::{Local, TimeZone};
+
+    let test_dir = TestDirectory::create("linux", "date");
+
+    let old_date: SystemTime = Local.with_ymd_and_hms(2003, 3, 3, 0, 0, 0).unwrap().into();
+    let med_date: SystemTime = Local
+        .with_ymd_and_hms(2006, 6, 15, 23, 14, 29)
+        .unwrap()
+        .into();
+    let new_date: SystemTime = Local
+        .with_ymd_and_hms(2009, 12, 22, 10, 38, 53)
+        .unwrap()
+        .into();
+
+    // Sleep between each create as we can not modified the created time
+    let peach = test_dir.create_file("peach");
+    thread::sleep(Duration::from_millis(100));
+    let peach_times = FileTimes::new()
+        .set_modified(med_date)
+        .set_accessed(new_date);
+    peach.set_times(peach_times).unwrap();
+
+    let plum = test_dir.create_file("plum");
+    thread::sleep(Duration::from_millis(100));
+    let plum_times = FileTimes::new()
+        .set_modified(new_date)
+        .set_accessed(old_date);
+    plum.set_times(plum_times).unwrap();
+
+    let pear = test_dir.create_file("pear");
+    let pear_times = FileTimes::new()
+        .set_modified(old_date)
+        .set_accessed(med_date);
+    pear.set_times(pear_times).unwrap();
+
+    test_dir.run_tests();
+}
+
+#[test]
+#[cfg(target_os = "linux")]
+fn cli_tests_linux_xattr() {
+    let test_dir = TestDirectory::create("linux", "xattr");
+
+    test_dir.create_files(&["file_no_attributes", "file_attributes"]);
+    test_dir.create_dirs(&["dir_no_attributes", "dir_attributes"]);
+
+    // command from package `attr`
+    test_dir.run(
+        "setfattr",
+        &[
+            "--name=user.xdg.tags",
+            "--value=foo,bar,baz",
+            "file_attributes",
+        ],
+    );
+
+    test_dir.run(
+        "setfattr",
+        &[
+            "--name=user.xdg.tags",
+            "--value=foo,bar,baz",
+            "dir_attributes",
+        ],
+    );
+
+    test_dir.run_tests();
+}
--- tests/cli_tests_unix.rs
+++ tests/cli_tests_unix.rs
@@ -4,52 +4,6 @@ use cli_tests_helpers::TestDirectory;
 
 #[test]
 #[cfg(unix)]
-// This test needs locales en_US, fr_FR and ja_JP.
-fn cli_tests_unix_date() {
-    use std::fs::FileTimes;
-    use std::thread;
-    use std::time::{Duration, SystemTime};
-
-    use chrono::{Local, TimeZone};
-
-    let test_dir = TestDirectory::create("unix", "date");
-
-    let old_date: SystemTime = Local.with_ymd_and_hms(2003, 3, 3, 0, 0, 0).unwrap().into();
-    let med_date: SystemTime = Local
-        .with_ymd_and_hms(2006, 6, 15, 23, 14, 29)
-        .unwrap()
-        .into();
-    let new_date: SystemTime = Local
-        .with_ymd_and_hms(2009, 12, 22, 10, 38, 53)
-        .unwrap()
-        .into();
-
-    // Sleep between each create as we can not modified the created time
-    let peach = test_dir.create_file("peach");
-    thread::sleep(Duration::from_millis(100));
-    let peach_times = FileTimes::new()
-        .set_modified(med_date)
-        .set_accessed(new_date);
-    peach.set_times(peach_times).unwrap();
-
-    let plum = test_dir.create_file("plum");
-    thread::sleep(Duration::from_millis(100));
-    let plum_times = FileTimes::new()
-        .set_modified(new_date)
-        .set_accessed(old_date);
-    plum.set_times(plum_times).unwrap();
-
-    let pear = test_dir.create_file("pear");
-    let pear_times = FileTimes::new()
-        .set_modified(old_date)
-        .set_accessed(med_date);
-    pear.set_times(pear_times).unwrap();
-
-    test_dir.run_tests();
-}
-
-#[test]
-#[cfg(unix)]
 #[cfg(feature = "git")]
 fn cli_tests_unix_git() {
     use std::io::Write;
@@ -139,36 +93,6 @@ fn cli_tests_unix_git() {
 
     new_staged_modified.write_all(b"a").unwrap();
     modified_staged_modified.write_all(b"a").unwrap();
-
-    test_dir.run_tests();
-}
-
-#[test]
-#[cfg(unix)]
-fn cli_tests_unix_xattr() {
-    let test_dir = TestDirectory::create("unix", "xattr");
-
-    test_dir.create_files(&["file_no_attributes", "file_attributes"]);
-    test_dir.create_dirs(&["dir_no_attributes", "dir_attributes"]);
-
-    // command from package `attr`
-    test_dir.run(
-        "setfattr",
-        &[
-            "--name=user.xdg.tags",
-            "--value=foo,bar,baz",
-            "file_attributes",
-        ],
-    );
-
-    test_dir.run(
-        "setfattr",
-        &[
-            "--name=user.xdg.tags",
-            "--value=foo,bar,baz",
-            "dir_attributes",
-        ],
-    );
 
     test_dir.run_tests();
 }
