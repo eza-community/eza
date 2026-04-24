9ebaaf9e3bb7bb8b45288f93c0921fb93122e100
Hannaeko <19394895+hannaeko@users.noreply.github.com>
2026-04-24 17:19:46 +0200

    improve script

--- devtools/regen.sh
+++ devtools/regen.sh
@@ -3,7 +3,7 @@ gid=$(stat -c '%g' tests/cmd)
 
 export TRYCMD=overwrite
 
-sudo chown -R eza_test: tests/cmd
+sudo chown -R $(whoami): tests/cmd
 
 cargo --locked test -- --test cli_tests --test-threads 1
 cargo --locked test --no-default-features -- --test cli_tests_any_no_git --test-threads 1
